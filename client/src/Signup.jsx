c97aaa096c233aeed54309306a9e49537a62f3c1
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-09-30 20:52:07 +0530

    Update Signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -11,7 +11,7 @@ function Signup(){
 
     const handleSubmit = (e) =>{
         e.preventDefault()
-    axios.post('http://localhost:3001/register',{name,email,password})
+    axios.post('https://mern-server-api-phi.vercel.app/register',{name,email,password})
     .then(result =>{
         console.log(result)
         navigate('/')
