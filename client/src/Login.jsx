eea578696ed2ee6675fab0c5585e22c167886ec2
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-09-30 20:52:50 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -10,7 +10,7 @@ function Login(){
 
     const handleSubmit = (e) =>{
         e.preventDefault()
-    axios.post('http://localhost:3001/login',{email,password})
+    axios.post('https://mern-server-api-phi.vercel.app/',{email,password})
     .then(result =>{
         console.log(result)
         if(result.data == 'success'){
