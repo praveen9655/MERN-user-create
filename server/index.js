2e42657cb2c5b5f8829289c991d87d86bb3f3766
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-09-30 20:55:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://mern-user-create.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
