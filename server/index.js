0deca0116c1c4d216d62eae3f053b9506e883ac1
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-09-30 21:53:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const RegisterModel = require('./models/Employee')
+const EmployeeModel = require('./models/Employee')
 
 const app = express()
 app.use(cors(
@@ -26,9 +26,27 @@ mongoose.connect(dbUrl, connectionParams)
 app.get("/", (req, res) => {
     res.json("Hello");
 })
+
+app.post('/login',(req,res)=>{
+    const{email,password}=req.body;
+    EmployeeModel.findOne({email: email})
+    .then(user=>{
+        if(user){
+            if(user.password === password){
+                res.json('success')
+            }
+            else{
+                res.json('The password is incorrect')
+            }
+        } else{
+            res.json('No record existed')
+        }
+    })
+})
+
 app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
+    EmployeeModel.findOne({email: email})
     .then(user => {
         if(user) {
             res.json("Already have an account")
